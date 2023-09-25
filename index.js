08a48b11b6750be1cad521c122a021e7d5981f81
TedKelvin <tedkelvin19@gmail.com>
2023-09-26 00:47:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers,name){
+    return drivers.filter((driverName)=>driverName.toLowerCase()=== name.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers,intials){
+    return drivers.filter((driverName)=> driverName.charAt(0)===intials.charAt(0))
+}
+
+function matchName(drivers,home){
+    return drivers.filter((driverName)=> driverName.name===home)
+}
